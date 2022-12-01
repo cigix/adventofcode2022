36fff1caab9d989a3e24753a654f93de59cd838a
cigix <cigix@cigix.me>
2022-12-01 10:25:55 +0100

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -5,12 +5,15 @@ import sys
 if len(sys.argv) < 2:
     exit(1)
 
+elves = [0]
 with open(sys.argv[1]) as f:
-    elves = [0]
     for line in f.readlines():
         if line.strip():
             elves[-1] += int(line.strip())
         else:
             elves.append(0)
 
-print(max(elves))
+#print(max(elves)) # part 1
+
+elves = sorted(elves)
+print(sum(elves[-3:])) # part 2
