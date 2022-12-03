a8dba7f048da39f4e5e795a2155dc5855b03b3f3
cigix <cigix@cigix.me>
2022-12-03 11:20:16 +0100

    day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -8,14 +8,24 @@ if len(sys.argv) < 2:
 with open(sys.argv[1]) as f:
     rucksacks = list(map(str.strip, f))
 
+def priority(item):
+    if item.islower():
+        return ord(item) - ord('a') + 1
+    else:
+        return ord(item) - ord('A') + 27
+
 priorities = 0
 for rucksack in rucksacks:
     l = len(rucksack) // 2
     compartments = set(rucksack[:l]), set(rucksack[l:])
     for item in compartments[0] & compartments[1]:
-        if item.islower():
-            priorities += ord(item) - ord('a') + 1
-        else:
-            priorities += ord(item) - ord('A') + 27
+        priorities += priority(item)
 
-print(priorities) # part 1
+#print(priorities) # part 1
+
+badges = 0
+for i in range(0, len(rucksacks), 3):
+    for badge in set(rucksacks[i]) & set(rucksacks[i + 1]) & set(rucksacks[i + 2]):
+        badges += priority(badge)
+
+print(badges) # part 2
