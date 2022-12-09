9249009b59cd4c403b8a1ccbe85e5368003bf4d7
cigix <cigix@cigix.me>
2022-12-09 10:41:30 +0100

    day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -10,33 +10,37 @@ with open(sys.argv[1]) as f:
         (m, int(c))
         for m, c in map(str.split, map(str.strip, f))]
 
-H = (0, 0)
-T = (0, 0)
-trace = { T }
+knots = [(0, 0)] * 10
 
-def updateT():
-    global H, T, trace
-    dx = H[0] - T[0]
-    dy = H[1] - T[1]
+# update knots[k] from knots[k - 1]
+def updateKnot(k):
+    global knots
+    dx = knots[k - 1][0] - knots[k][0]
+    dy = knots[k - 1][1] - knots[k][1]
     if -1 <= dx <= 1 and -1 <= dy <= 1:
-        # T touches H, no movement
+        # knots[k] touches knots[k - 1], no movement
         return
     # Clamp between -1 and 1 => ensures diagonal movement
     dx = max(-1, min(1, dx))
     dy = max(-1, min(1, dy))
-    T = T[0] + dx, T[1] + dy
-    trace.add(T)
+    knots[k] = knots[k][0] + dx, knots[k][1] + dy
 
+trace1 = { knots[1] }
+traceT = { knots[-1] }
 for move, count in moves:
     for _ in range(count):
         if move == 'L':
-            H = H[0] - 1, H[1]
+            knots[0] = knots[0][0] - 1, knots[0][1]
         elif move == 'U':
-            H = H[0], H[1] - 1
+            knots[0] = knots[0][0], knots[0][1] - 1
         elif move == 'R':
-            H = H[0] + 1, H[1]
+            knots[0] = knots[0][0] + 1, knots[0][1]
         elif move == 'D':
-            H = H[0], H[1] + 1
-        updateT()
+            knots[0] = knots[0][0], knots[0][1] + 1
+        for k in range(1, len(knots)):
+            updateKnot(k)
+        trace1.add(knots[1])
+        traceT.add(knots[-1])
 
-print(len(trace)) # part 1
+print(len(trace1)) # part 1
+print(len(traceT)) # part 2
