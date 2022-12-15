93318fa971fa39e30c81a0d52edc9cc50d16cf04
cigix <cigix@cigix.me>
2022-12-15 23:22:46 +0100

    day 15 part 2

--- day15/day15.rs
+++ day15/day15.rs
@@ -6,11 +6,11 @@ use regex::Regex;
 
 #[derive(Debug, Eq, PartialEq)]
 struct Point {
-    x: i32,
-    y: i32
+    x: i64,
+    y: i64
 }
 
-fn manhattan(p1: &Point, p2: &Point) -> u32
+fn manhattan(p1: &Point, p2: &Point) -> u64
 {
     p1.x.abs_diff(p2.x) + p1.y.abs_diff(p2.y)
 }
@@ -24,14 +24,14 @@ struct Beacon {
 struct Sensor {
     pos: Point,
     _closest: Rc<Beacon>,
-    distance: u32
+    distance: u64
 }
 
 fn main() -> ()
 {
     // Panics if no 1st or 2nd arg, and if 2nd arg is not an i32
     let filename = env::args().nth(1).unwrap();
-    let queryrow = env::args().nth(2).unwrap().parse::<i32>().unwrap();
+    let queryrow = env::args().nth(2).unwrap().parse::<i64>().unwrap();
 
     let mut beacons: Vec<Rc<Beacon>> = Vec::new();
     let mut sensors: Vec<Sensor> = Vec::new();
@@ -43,10 +43,10 @@ fn main() -> ()
         .split_terminator('\n')
     {
         let captures = re.captures(line).unwrap();
-        let sx = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
-        let sy = captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
-        let bx = captures.get(3).unwrap().as_str().parse::<i32>().unwrap();
-        let by = captures.get(4).unwrap().as_str().parse::<i32>().unwrap();
+        let sx = captures.get(1).unwrap().as_str().parse::<i64>().unwrap();
+        let sy = captures.get(2).unwrap().as_str().parse::<i64>().unwrap();
+        let bx = captures.get(3).unwrap().as_str().parse::<i64>().unwrap();
+        let by = captures.get(4).unwrap().as_str().parse::<i64>().unwrap();
 
         let sensorpos = Point{ x: sx, y: sy };
         let beaconpos = Point{ x: bx, y: by };
@@ -79,18 +79,53 @@ fn main() -> ()
              // solving for lowest possible value for p2.x
              // |s.pos.x - x| = s.distance - |s.pos.y - queryrow|
              // x = s.pos.x - (s.distance - |s.pos.y - queryrow|)
-             s.pos.x - (s.distance - s.pos.y.abs_diff(queryrow)) as i32)
+             s.pos.x - (s.distance - s.pos.y.abs_diff(queryrow)) as i64)
         .min()
         .unwrap();
     let maxx = sensors.iter()
         .filter(|&s| queryrow.abs_diff(s.pos.y) <= s.distance)
-        .map(|s| s.pos.x + (s.distance - s.pos.y.abs_diff(queryrow)) as i32)
+        .map(|s| s.pos.x + (s.distance - s.pos.y.abs_diff(queryrow)) as i64)
         .max()
         .unwrap();
     let beaconsonqueryrow = beacons.iter()
         .filter(|&rcb| rcb.pos.y == queryrow)
         .count();
-    println!("{}", maxx - minx + 1 - beaconsonqueryrow as i32);
+    println!("{}", maxx - minx + 1 - beaconsonqueryrow as i64);
+
+    // part 2
+    // Only 1 solution for distress beacon => must be touching a diamond
+    //let size = 20;
+    let size = 4_000_000;
+'outer: for sensor in sensors.iter() {
+        // Loop on all candidate y positions
+        let mindy = - (sensor.distance as i64) - 1;
+        let maxdy = sensor.distance as i64 + 1;
+        for dy in mindy ..= maxdy {
+            let y = sensor.pos.y + dy;
+            if y < 0 || size < y {
+                continue; // this y value is outside the square
+            }
+            let absdx = sensor.distance as i64 - dy.abs() + 1;
+            // Loop on both possible directions
+            for direction in (-1 ..= 1).step_by(2) {
+                let dx = absdx * direction;
+                let x = sensor.pos.x + dx;
+                if x < 0 || size < x {
+                    continue; // this x value is outside the square
+                }
+                
+                let pos = Point{ x, y };
+                // if all sensors sense below their distance to pos
+                if sensors.iter()
+                    .all(|s| s.distance < manhattan(&pos, &s.pos))
+                {
+                    // then no sensor would sense a beacon here
+                    println!("{:2}, {:2}: {}", x, y, x * 4000000 + y);
+                    break 'outer;
+                }
+            }
+        }
+    }
 
     // Visualiser
     //let minx = sensors.iter().map(|s| s.pos.x - s.distance as i32).min().unwrap();
@@ -100,22 +135,27 @@ fn main() -> ()
     //for y in miny..=maxy {
     //    print!("{:3} ", y);
     //    for x in minx..=maxx {
+    //        if x == 0 && y == 0 {
+    //            print!("0");
+    //            continue;
+    //        }
+
     //        let pos = Point{ x, y };
-    //        
+
     //        if let Some(_) = beacons.iter().find(|&rcb| rcb.pos == pos) {
     //            print!("B");
+    //            continue;
+    //        }
+    //        if let Some(_) = sensors.iter().find(|&s| s.pos == pos) {
+    //            print!("S");
+    //            continue;
+    //        } 
+    //        if sensors.iter()
+    //            .any(|s| manhattan(&pos, &s.pos) <= s.distance)
+    //        {
+    //            print!("#");
     //        } else {
-    //            if let Some(_) = sensors.iter().find(|&s| s.pos == pos) {
-    //                print!("S");
-    //            } else {
-    //                if sensors.iter()
-    //                    .any(|s| manhattan(&pos, &s.pos) <= s.distance)
-    //                {
-    //                    print!("#");
-    //                } else {
-    //                    print!(".");
-    //                }
-    //            }
+    //            print!(".");
     //        }
     //    }
     //    println!();
