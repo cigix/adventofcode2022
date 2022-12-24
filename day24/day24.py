9e70262c5d65e014d1155564e6f16623222c44ff
cigix <cigix@cigix.me>
2022-12-24 18:34:39 +0100

    day 24 part 2

--- day24/day24.py
+++ day24/day24.py
@@ -16,56 +16,66 @@ for y, line in enumerate(lines):
         if c in ">v<^":
             blizzards.add((x, y, c))
 
-step = 0
-positions = { startpos }
-while True:
-    newblizzards = set()
-    for x, y, direction in blizzards:
-        if direction == '>':
-            if lines[y][x + 1] == '#':
-                while lines[y][x - 1] != '#':
-                    x -= 1
-            else:
-                x += 1
-        elif direction == '<':
-            if lines[y][x - 1] == '#':
-                while lines[y][x + 1] != '#':
+def go(start, end):
+    global blizzards
+    step = 0
+    positions = { start }
+    while True:
+        newblizzards = set()
+        for x, y, direction in blizzards:
+            if direction == '>':
+                if lines[y][x + 1] == '#':
+                    while lines[y][x - 1] != '#':
+                        x -= 1
+                else:
                     x += 1
-            else:
-                x -= 1
-        elif direction == 'v':
-            if lines[y + 1][x] == '#':
-                while lines[y - 1][x] != '#':
-                    y -= 1
-            else:
-                y += 1
-        elif direction == '^':
-            if lines[y - 1][x] == '#':
-                while lines[y + 1][x] != '#':
+            elif direction == '<':
+                if lines[y][x - 1] == '#':
+                    while lines[y][x + 1] != '#':
+                        x += 1
+                else:
+                    x -= 1
+            elif direction == 'v':
+                if lines[y + 1][x] == '#':
+                    while lines[y - 1][x] != '#':
+                        y -= 1
+                else:
                     y += 1
-            else:
-                y -= 1
-        newblizzards.add((x, y, direction))
+            elif direction == '^':
+                if lines[y - 1][x] == '#':
+                    while lines[y + 1][x] != '#':
+                        y += 1
+                else:
+                    y -= 1
+            newblizzards.add((x, y, direction))
 
-    newblizzardspositions = set(
-        map(
-            lambda b: (b[0], b[1]),
-            newblizzards))
+        newblizzardspositions = set(
+            map(
+                lambda b: (b[0], b[1]),
+                newblizzards))
 
-    newpositions = set()
-    for x, y in positions:
-        for dx, dy in ((0, 0), (0, 1), (1, 0), (0, -1), (-1, 0)):
-            newx, newy = x + dx, y + dy
-            if lines[newy][newx] == '#':
-                continue # reject that candidate
-            if (newx, newy) in newblizzardspositions:
-                continue # reject that candidate
-            newpositions.add((newx, newy))
+        newpositions = set()
+        for x, y in positions:
+            for dx, dy in ((0, 0), (0, 1), (1, 0), (0, -1), (-1, 0)):
+                newx, newy = x + dx, y + dy
+                if not (0 <= newx < len(lines[0]) and 0 <= newy < len(lines)):
+                    continue
+                if lines[newy][newx] == '#':
+                    continue # reject that candidate
+                if (newx, newy) in newblizzardspositions:
+                    continue # reject that candidate
+                newpositions.add((newx, newy))
 
-    blizzards = newblizzards
-    positions = newpositions
-    step += 1
+        blizzards = newblizzards
+        positions = newpositions
+        step += 1
 
-    if endpos in newpositions:
-        print(step)
-        exit(0)
+        if end in newpositions:
+            return step
+
+
+stepcount = go(startpos, endpos)
+print(stepcount) # part 1
+stepcount += go(endpos, startpos)
+stepcount += go(startpos, endpos)
+print(stepcount) # part 2
